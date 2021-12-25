6e6e72614f96d16c6b95dab79a6779d80867e286
puchengyu2009 <puchengyu2009@gmail.com>
2021-12-25 15:39:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
-sed -i "s/OpenWrt /MikePu compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
